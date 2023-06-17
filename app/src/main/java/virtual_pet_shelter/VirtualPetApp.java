6719bcb64dc8ef0f684d705577dafae5255d9ac7
Alyssa Diamant <alyssadiamant4@gmail.com>
2023-06-17 17:00:11 -0400

    methods & arraylist added, next functionality

--- app/src/main/java/virtual_pet_shelter/VirtualPetApp.java
+++ app/src/main/java/virtual_pet_shelter/VirtualPetApp.java
@@ -4,5 +4,6 @@ public class VirtualPetApp {
     public static void main(String[] args) {
         // don't forget about the tick()
         // don't forget to add a read me
+        // methods added, arraylist created
     }
 }
